396e2ab895a6f8497fef4e69af61add23b6ebd52
Abderrahmane07 <75677650+Abderrahmane07@users.noreply.github.com>
2022-04-23 04:51:37 +0200

    Fully working now with an minor exception to handle!

--- lib/services/remote_service.dart
+++ lib/services/remote_service.dart
@@ -12,19 +12,23 @@ class RemoteService {
   var client = http.Client();
 
   Future<List> getArticle(List<bool> inheritedList) async {
-    List<String> finalOne = linkGenerator(inheritedList);
+    List titleAndDomaine = await titlePicker(inheritedList);
+    String title = titleAndDomaine[0];
+    String domaine = titleAndDomaine[1];
 
-    var uri = Uri.parse(finalOne[0]);
+    String finalCallLink = linkGenerator(title);
+
+    var uri = Uri.parse(finalCallLink);
     var response = await client.get(uri);
     if (response.statusCode == 200) {
       var json = response.body;
-      return [articleFromJson(json), finalOne[1]];
+      return [articleFromJson(json), domaine];
     } else {
       throw const Text('Error in RemoteService Class');
     }
   }
 
-  Future<Category> categorySelector(List<bool> inheritedList) async {
+  Future<List> categorySelector(List<bool> inheritedList) async {
     // var client = http.Client();
     List<int> testListToInt = [];
     for (var i = 0; i < inheritedList.length; i++) {
@@ -40,26 +44,28 @@ class RemoteService {
       domaine = domaines[testListToInt[random.nextInt(testListToInt.length)]];
     }
     String url =
-        'https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:$domaine&cmlimit=max&cmnamespace=14';
+        'https://en.wikipedia.org/w/api.php?format=json&action=query&list=categorymembers&cmtitle=Category:$domaine&cmlimit=max&cmnamespace=14&from';
     var uri = Uri.parse(url);
     var response = await client.get(uri);
     if (response.statusCode == 200) {
       var json = response.body;
-      return categoryFromJson(json);
+      return [categoryFromJson(json), domaine];
     } else {
       throw const Text('Error in RemoteService Class - categorySelector');
     }
   }
 
-  Future<String> titlePicker(List<bool> inheritedList) async {
-    Category selectedSubCategory = await categorySelector(inheritedList);
+  Future<List> titlePicker(List<bool> inheritedList) async {
+    List result = await categorySelector(inheritedList);
+    Category selectedSubCategory = result[0];
+    String domaine = result[1];
 
     String selectedCategory = (selectedSubCategory.query.categorymembers)[
             random.nextInt((selectedSubCategory.query.categorymembers).length)]
         .title;
     // List selectedCategories = (selectedSubCategory.query.categorymembers)[0].title;
     String url =
-        'https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=$selectedCategory&cmlimit=max&cmnamespace=0';
+        'https://en.wikipedia.org/w/api.php?format=json&action=query&list=categorymembers&cmtitle=$selectedCategory&cmlimit=max&cmnamespace=0';
     var uri = Uri.parse(url);
     var response = await client.get(uri);
     Category jsonOfSubCategory;
@@ -72,19 +78,17 @@ class RemoteService {
     String selectedTitle = (jsonOfSubCategory.query.categorymembers)[
             random.nextInt((jsonOfSubCategory.query.categorymembers).length)]
         .title;
-    return selectedTitle;
+    return [selectedTitle, domaine];
   }
 
-  List<String> linkGenerator(String givenTitle) {
-    String domaine;
-
+  String linkGenerator(String givenTitle) {
     // This to take a link and give the title
     // String wikiModi = wikiLink.replaceRange(0, 30, "");
+    givenTitle.replaceAll(' ', '%20');
 
     String generalLink =
         'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&titles=$givenTitle';
-    //String generalLink = apiCallLink + wikiModi;
-    return [generalLink.replaceAll('_', '%20'), domaine];
+    return generalLink.replaceAll('_', '%20');
   }
 
 // This function takes the list of booleens that determine the preferences of the user
@@ -123,81 +127,81 @@ class RemoteService {
   // }
 }
 
-const Map<String, List<String>> links = {
-  'Astronomy': [
-    'https://en.wikipedia.org/wiki/Constellation',
-    'https://en.wikipedia.org/wiki/Cosmic_wind',
-    'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
-  ],
-  'Bockchain': [
-    'https://en.wikipedia.org/wiki/Blockchain',
-    'https://en.wikipedia.org/wiki/Privacy_and_blockchain',
-    'https://en.wikipedia.org/wiki/Cryptocurrency'
-  ],
-  'Mathematics': [
-    'https://en.wikipedia.org/wiki/Number_theory',
-    'https://en.wikipedia.org/wiki/Geometry',
-    'https://en.wikipedia.org/wiki/Algebra'
-  ],
-  'Science': [
-    'https://en.wikipedia.org/wiki/Science',
-    'https://en.wikipedia.org/wiki/Research',
-    'https://en.wikipedia.org/wiki/Outline_of_natural_science'
-  ],
-  'History': [
-    'https://en.wikipedia.org/wiki/History',
-    'https://en.wikipedia.org/wiki/World_War_II',
-    'https://en.wikipedia.org/wiki/Hirohito'
-  ],
-  // 'Religion': [
-  //   'https://en.wikipedia.org/wiki/Wahhabism',
-  //   'https://en.wikipedia.org/wiki/Wahhabism',
-  //   'https://en.wikipedia.org/wiki/Wahhabism'
-  // ],
-  'Medicine': [
-    'https://en.wikipedia.org/wiki/Constellation',
-    'https://en.wikipedia.org/wiki/Cosmic_wind',
-    'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
-  ],
-  'Art': [
-    'https://en.wikipedia.org/wiki/Constellation',
-    'https://en.wikipedia.org/wiki/Cosmic_wind',
-    'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
-  ],
-  'Sport': [
-    'https://en.wikipedia.org/wiki/Constellation',
-    'https://en.wikipedia.org/wiki/Cosmic_wind',
-    'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
-  ],
-  'Technology': [
-    'https://en.wikipedia.org/wiki/Constellation',
-    'https://en.wikipedia.org/wiki/Cosmic_wind',
-    'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
-  ],
-  'Biology': [
-    'https://en.wikipedia.org/wiki/Constellation',
-    'https://en.wikipedia.org/wiki/Cosmic_wind',
-    'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
-  ],
-  'Geography': [
-    'https://en.wikipedia.org/wiki/Constellation',
-    'https://en.wikipedia.org/wiki/Cosmic_wind',
-    'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
-  ],
-  'Finance': [
-    'https://en.wikipedia.org/wiki/Constellation',
-    'https://en.wikipedia.org/wiki/Cosmic_wind',
-    'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
-  ],
-  'Architecture': [
-    'https://en.wikipedia.org/wiki/Constellation',
-    'https://en.wikipedia.org/wiki/Cosmic_wind',
-    'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
-  ],
-  'Random': [
-    // it's this one that is used
-    'https://en.wikipedia.org/wiki/Albert_Einstein',
-    'https://en.wikipedia.org/wiki/Randomness',
-    'https://en.wikipedia.org/wiki/Complexity',
-  ],
-};
+// const Map<String, List<String>> links = {
+//   'Astronomy': [
+//     'https://en.wikipedia.org/wiki/Constellation',
+//     'https://en.wikipedia.org/wiki/Cosmic_wind',
+//     'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
+//   ],
+//   'Bockchain': [
+//     'https://en.wikipedia.org/wiki/Blockchain',
+//     'https://en.wikipedia.org/wiki/Privacy_and_blockchain',
+//     'https://en.wikipedia.org/wiki/Cryptocurrency'
+//   ],
+//   'Mathematics': [
+//     'https://en.wikipedia.org/wiki/Number_theory',
+//     'https://en.wikipedia.org/wiki/Geometry',
+//     'https://en.wikipedia.org/wiki/Algebra'
+//   ],
+//   'Science': [
+//     'https://en.wikipedia.org/wiki/Science',
+//     'https://en.wikipedia.org/wiki/Research',
+//     'https://en.wikipedia.org/wiki/Outline_of_natural_science'
+//   ],
+//   'History': [
+//     'https://en.wikipedia.org/wiki/History',
+//     'https://en.wikipedia.org/wiki/World_War_II',
+//     'https://en.wikipedia.org/wiki/Hirohito'
+//   ],
+//   // 'Religion': [
+//   //   'https://en.wikipedia.org/wiki/Wahhabism',
+//   //   'https://en.wikipedia.org/wiki/Wahhabism',
+//   //   'https://en.wikipedia.org/wiki/Wahhabism'
+//   // ],
+//   'Medicine': [
+//     'https://en.wikipedia.org/wiki/Constellation',
+//     'https://en.wikipedia.org/wiki/Cosmic_wind',
+//     'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
+//   ],
+//   'Art': [
+//     'https://en.wikipedia.org/wiki/Constellation',
+//     'https://en.wikipedia.org/wiki/Cosmic_wind',
+//     'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
+//   ],
+//   'Sport': [
+//     'https://en.wikipedia.org/wiki/Constellation',
+//     'https://en.wikipedia.org/wiki/Cosmic_wind',
+//     'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
+//   ],
+//   'Technology': [
+//     'https://en.wikipedia.org/wiki/Constellation',
+//     'https://en.wikipedia.org/wiki/Cosmic_wind',
+//     'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
+//   ],
+//   'Biology': [
+//     'https://en.wikipedia.org/wiki/Constellation',
+//     'https://en.wikipedia.org/wiki/Cosmic_wind',
+//     'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
+//   ],
+//   'Geography': [
+//     'https://en.wikipedia.org/wiki/Constellation',
+//     'https://en.wikipedia.org/wiki/Cosmic_wind',
+//     'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
+//   ],
+//   'Finance': [
+//     'https://en.wikipedia.org/wiki/Constellation',
+//     'https://en.wikipedia.org/wiki/Cosmic_wind',
+//     'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
+//   ],
+//   'Architecture': [
+//     'https://en.wikipedia.org/wiki/Constellation',
+//     'https://en.wikipedia.org/wiki/Cosmic_wind',
+//     'https://en.wikipedia.org/wiki/Milky_Way_(mythology)'
+//   ],
+//   'Random': [
+//     // it's this one that is used
+//     'https://en.wikipedia.org/wiki/Albert_Einstein',
+//     'https://en.wikipedia.org/wiki/Randomness',
+//     'https://en.wikipedia.org/wiki/Complexity',
+//   ],
+// };
